3394fd52e2adefae18a15e71d40efcc40ac671a6
Aaron Myburgh <amyburgh@42.us.org>
2018-06-06 16:37:49 -0700

    get_next_line v1

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amyburgh <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/05/24 22:03:08 by amyburgh          #+#    #+#             */
+/*   Updated: 2018/06/05 20:50:43 by amyburgh         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stddef.h>
+# define BUFF_SIZE 32
+
+int		get_next_line(const int fd, char **line);
+
+#endif
